9ac4aaae11272264918dddbda72cdb78eafc609d
wilfr <wilfredvancasteren@hotmail.com>
2025-08-07 16:28:32 +0200

    2 models

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ from openai import OpenAI
 
 client = OpenAI(base_url="http://192.168.178.25:1234/v1", api_key="lm-studio")
 
+
+
 completion = client.chat.completions.create(
-  model="qwen/qwen3-8b",
+  #model = "qwen/qwen3-8b",
+  model="gemma-1.1-2b-it",
   messages=[
     {"role": "system", "content": "Always answer in rhymes."},
     {"role": "user", "content": "Introduce yourself."}
